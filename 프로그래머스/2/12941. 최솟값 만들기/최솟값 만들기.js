deb28d757e15a81d83433ec7cf557868dd699980
MIN JI-YEONG <kuck523@gmail.com>
2024-10-02 03:23:05 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.74 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,15 +1,7 @@
 function solution(A, B) {
-  const { hasMaxArr, Arr } =
-    Math.max(...A) > Math.max(...B)
-      ? { hasMaxArr: A, Arr: B }
-      : { hasMaxArr: B, Arr: A };
+  A.sort((a, b) => b - a); // 내림차순 100->1
+  B.sort((a, b) => a - b); // 오름차순 1->100
 
-  hasMaxArr.sort((a, b) => b - a); // 내림차순 100->1
-  Arr.sort((a, b) => a - b); // 오름차순 1->100
-
-  // 최소합을 구하기 위해 최댓값이랑 최솟값이랑 곱하기
-  const sum = hasMaxArr.reduce((acc, item, idx) => {
-    return acc + item * Arr[idx];
-  }, 0);
+  const sum = A.reduce((acc, a, idx) => acc + a * B[idx], 0);
   return sum;
 }
