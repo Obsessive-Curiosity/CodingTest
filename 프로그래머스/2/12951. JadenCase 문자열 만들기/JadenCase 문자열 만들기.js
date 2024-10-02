b68be278dcb3f93f13f5ad37cb9558ac91cb39a5
MIN JI-YEONG <kuck523@gmail.com>
2024-10-02 16:31:45 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,7 +1,7 @@
 function solution(s) {
   return s
-    .toLowerCase() // 전체 소문자 전환
-    .split(" ") // 공백으로 분리하기
+    .toLowerCase()
+    .split(" ")
     .map((word) => {
       if (word.length > 0 && /[a-zA-Z]/.test(word[0])) {
         return word[0].toUpperCase() + word.slice(1);
