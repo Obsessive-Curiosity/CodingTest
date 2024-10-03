139f7980fdab4e01c4b12b70c9742b6981a717d5
MIN JI-YEONG <kuck523@gmail.com>
2024-10-03 13:28:15 +0900

    [level 1] Title: 숫자 짝꿍, Time: 2356.32 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,21 +1,27 @@
+// 문자 갯수 세기
 function countChar(str, char) {
   return [...str].reduce((count, c) => (c === char ? count + 1 : count), 0);
 }
 
 function solution(X, Y) {
-  const xSet = new Set([...X]);
+  // 길이가 짧은 문자열 Set 만들기
+  const set = X.length > Y.length ? new Set([...Y]) : new Set([...X]);
 
-  const arr = [...xSet].reduce((acc, cur) => {
+  // 최소 갯수 구하기
+  const arr = [...set].reduce((acc, cur) => {
     const cntX = countChar(X, cur);
     const cntY = countChar(Y, cur);
     const minCnt = Math.min(cntX, cntY);
+
     if (minCnt > 0) acc.push([cur, minCnt]);
 
     return acc;
   }, []);
 
+  // 내림차순으로 sort
   arr.sort((a, b) => b[0] - a[0]);
 
+  // repeat로 "00"->"0" 필터링
   const ret = arr.reduce((acc, cur, idx) => {
     if (idx === 0 && cur[0] === "0" && cur[1] > 1) {
       acc += cur[0].repeat(1);
@@ -25,5 +31,6 @@ function solution(X, Y) {
     return acc;
   }, "");
 
-  return ret === "" ? "-1" : `${ret}`;
+  // 결과 반환
+  return ret === "" ? "-1" : ret;
 }
