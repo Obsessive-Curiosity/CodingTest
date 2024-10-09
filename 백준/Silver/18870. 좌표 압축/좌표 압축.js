2cce10c44b52f58cce47a226a07efda3f3ab5e14
MIN JI-YEONG <kuck523@gmail.com>
2024-10-09 21:55:29 +0900

    [Silver II] Title: 좌표 압축, Time: 1404 ms, Memory: 310980 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.js
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.js
@@ -1,11 +1,60 @@
+// 오름차순 힙 정렬
+function heapSort(arr) {
+  // 힙을 구성하는 함수
+  function buildHeap(arr) {
+    const len = arr.length;
+
+    for (let i = Math.floor(len / 2) - 1; i >= 0; i--) {
+      heapify(arr, len, i);
+    }
+  }
+
+  // 최대 힙 사용
+  function heapify(arr, len, idx) {
+    let largest = idx;
+    const left = 2 * idx + 1;
+    const right = 2 * idx + 2;
+
+    if (left < len && arr[left] > arr[largest]) {
+      largest = left;
+    }
+    if (right < len && arr[right] > arr[largest]) {
+      largest = right;
+    }
+
+    if (idx !== largest) {
+      [arr[idx], arr[largest]] = [arr[largest], arr[idx]]; // swap
+      heapify(arr, len, largest);
+    }
+  }
+
+  // 정렬을 수행하는 함수
+  function sort(arr) {
+    const len = arr.length;
+
+    // 1단계
+    buildHeap(arr);
+
+    // 2, 3단계
+    for (let i = len - 1; i >= 0; i--) {
+      [arr[0], arr[i]] = [arr[i], arr[0]]; // swap
+      heapify(arr, i, 0);
+    }
+
+    return arr;
+  }
+
+  return sort(arr);
+}
+
 function solution(input) {
   const N = Number(input.shift());
   const arr = input.shift().split(" ").map(Number);
   const setArr = Array.from(new Set(arr));
+  const sortedArr = heapSort(setArr);
   const MapArr = new Map();
 
-  setArr.sort((a, b) => a - b); // 명시하지 않으면 문자열로 계산
-  setArr.forEach((num, idx) => MapArr.set(num, idx));
+  sortedArr.forEach((num, idx) => MapArr.set(num, idx));
 
   const result = arr.map((num) => MapArr.get(num)).join(" ");
   console.log(result);
