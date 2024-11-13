27d473eb7269500a2b2144e3fd0155fab104d8d5
MIN JI-YEONG <kuck523@gmail.com>
2024-11-14 01:29:32 +0900

    [Silver V] Title: 기술 연계마스터 임스, Time: 172 ms, Memory: 11872 KB -BaekjoonHub

--- 백준/Silver/25497. 기술 연계마스터 임스/기술 연계마스터 임스.js
+++ 백준/Silver/25497. 기술 연계마스터 임스/기술 연계마스터 임스.js
@@ -1,57 +1,26 @@
-function countSkills(skills) {
-  const stack = [];
-  let count = 0;
-
-  for (const skill of skills) {
-    stack.push(skill);
-    if (stack.length === 1) continue;
-    const target = stack.slice(-2).join("");
-    if (target === "SK" || target === "LR") {
-      count++;
-      stack.pop();
-      stack.pop();
-    }
-  }
-
-  return count;
-}
-
 function solution(input) {
   const N = Number(input.shift());
   const skills = input.shift();
-  const [numSkills, SKSkills, LRSkills] = [[], [], []];
-  let [scnt, kcnt, lcnt, rcnt] = [0, 0, 0, 0];
+  let [numcnt, scnt, kcnt, lcnt, rcnt] = [0, 0, 0, 0, 0];
   let count = 0;
 
   for (const skill of skills) {
     if (skill === "S") {
-      SKSkills.push(skill);
       scnt++;
     } else if (skill === "L") {
-      LRSkills.push(skill);
       lcnt++;
     } else if (skill === "K") {
-      if (scnt > kcnt) {
-        SKSkills.push(skill);
-        kcnt++;
-      } else {
-        break;
-      }
+      if (scnt > kcnt) kcnt++;
+      else break;
     } else if (skill === "R") {
-      if (lcnt > rcnt) {
-        LRSkills.push(skill);
-        rcnt++;
-      } else {
-        break;
-      }
-    } else {
-      numSkills.push(skill);
-    }
+      if (lcnt > rcnt) rcnt++;
+      else break;
+    } else numcnt++;
   }
 
-  count = numSkills.length;
-  count += countSkills(SKSkills);
-  count += countSkills(LRSkills);
+  count = numcnt;
+  count += Math.min(scnt, kcnt);
+  count += Math.min(lcnt, rcnt);
 
   console.log(count);
 }
