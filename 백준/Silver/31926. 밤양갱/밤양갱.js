bdab645365f2e07ccab1f0774988df08c56fe4e3
MIN JI-YEONG <kuck523@gmail.com>
2024-11-14 00:53:08 +0900

    [Silver I] Title: 밤양갱, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Silver/31926. 밤양갱/밤양갱.js
+++ 백준/Silver/31926. 밤양갱/밤양갱.js
@@ -9,8 +9,7 @@ function solution(input) {
 }
 
 const fs = require("fs");
-const filePath =
-  process.platform === "linux" ? "/dev/stdin" : "241113_middle.txt";
+const filePath = "/dev/stdin";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const result = solution(input);
